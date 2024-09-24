83cee7f76a07975a5c9b2e82a14faaecf8c793be
cyrilliusg <kirillgaynov@gmail.com>
2024-09-24 23:53:03 +0300

    implemented three new models: 'Brand'; 'Model'; 'Configuration' and their migrations

--- car_park/vehicle/admin.py
+++ car_park/vehicle/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Vehicle
+from .models import Vehicle, Brand, Model, Configuration
 
-admin.site.register(Vehicle)
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'vehicle_type')
+    list_filter = ('brand', 'vehicle_type')
+
+@admin.register(Configuration)
+class ConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'tank_capacity', 'payload', 'seats_number')
+    list_filter = ('model',)
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('vin', 'configuration', 'release_year', 'price', 'transmission_type')
+    list_filter = ('configuration__model__brand', 'release_year', 'transmission_type')
--- car_park/vehicle/models.py
+++ car_park/vehicle/models.py
@@ -1,12 +1,52 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 # Create your models here.
 
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название бренда')
+    country = models.CharField(max_length=100, verbose_name='Страна')
+
+    def __str__(self):
+        return self.name
+
+
+class Model(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название модели')
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='models', verbose_name='Бренд')
+
+    VEHICLE_TYPE_CHOICES = [
+        ('Passenger', 'Легковой'),
+        ('Truck', 'Грузовой'),
+        ('Bus', 'Автобус'),
+        ('SUV', 'Внедорожник'),
+        ('Van', 'Фургон'),
+    ]
+
+    vehicle_type = models.CharField(max_length=50,
+                                    choices=VEHICLE_TYPE_CHOICES,
+                                    verbose_name='Тип транспортного средства')
+
+    def __str__(self):
+        return f"{self.brand.name} {self.name}"
+
+
+class Configuration(models.Model):
+    model = models.ForeignKey(Model, on_delete=models.CASCADE, related_name='configurations', verbose_name='Модель')
+    name = models.CharField(max_length=100, verbose_name='Название комплектации')
+    tank_capacity = models.DecimalField(max_digits=5, decimal_places=1, validators=[MinValueValidator(0.0)], verbose_name='Объем бака (л)')
+    payload = models.PositiveIntegerField(verbose_name='Грузоподъемность (кг)')
+    seats_number = models.PositiveIntegerField(verbose_name='Количество мест')
+
+    def __str__(self):
+        return f"{self.model} - {self.name}"
+
+
 class Vehicle(models.Model):
     id = models.AutoField(primary_key=True)
     vin = models.CharField(max_length=17, unique=True, verbose_name='VIN')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стоимость')
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.0)], verbose_name='Стоимость')
     release_year = models.PositiveIntegerField(verbose_name='Год выпуска')
     mileage = models.PositiveIntegerField(verbose_name='Пробег')
     color = models.CharField(max_length=50, verbose_name='Цвет')
@@ -31,6 +71,11 @@ class Vehicle(models.Model):
         default=MECHANICAL,
         verbose_name='Тип трансмиссии'
     )
+
+    configuration = models.ForeignKey(Configuration,
+                                      on_delete=models.CASCADE,
+                                      related_name='vehicles',
+                                      verbose_name='Комплектация')
 
     def __str__(self):
         return f"{self.release_year} Vehicle"
