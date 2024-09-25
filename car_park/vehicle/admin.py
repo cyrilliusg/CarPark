571d27f67d15fd32a02b6162e6883b1df9cadfaa
cyrilliusg <kirillgaynov@gmail.com>
2024-09-25 17:35:05 +0300

    added 'id' field to display in models data in admin panel

--- car_park/vehicle/admin.py
+++ car_park/vehicle/admin.py
@@ -6,19 +6,19 @@ from .models import Vehicle, Brand, Model, Configuration
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ('name', 'country')
+    list_display = ('id', 'name', 'country')
 
 @admin.register(Model)
 class ModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'brand', 'vehicle_type')
+    list_display = ('id', 'name', 'brand', 'vehicle_type')
     list_filter = ('brand', 'vehicle_type')
 
 @admin.register(Configuration)
 class ConfigurationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'model', 'tank_capacity', 'payload', 'seats_number')
+    list_display = ('id', 'name', 'model', 'tank_capacity', 'payload', 'seats_number')
     list_filter = ('model',)
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ('vin', 'configuration', 'release_year', 'price', 'transmission_type')
+    list_display = ('id', 'vin', 'configuration', 'release_year', 'price', 'transmission_type')
     list_filter = ('configuration__model__brand', 'release_year', 'transmission_type')
