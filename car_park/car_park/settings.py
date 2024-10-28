7423e407883cea780bfc1fdb6b315fe38d967c6f
cyrilliusg <kirillgaynov@gmail.com>
2024-10-28 19:11:16 +0300

    implemented JWT authentication

--- car_park/car_park/settings.py
+++ car_park/car_park/settings.py
@@ -125,7 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         # Вы можете добавить другие методы аутентификации при необходимости
     ],
     'DEFAULT_PERMISSION_CLASSES': [
--- car_park/vehicle/urls.py
+++ car_park/vehicle/urls.py
@@ -1,7 +1,9 @@
 # urls.py
-from django.urls import path
+from django.urls import path, include
 from .views import VehicleListAPIView, EnterpriseListAPIView, DriverListAPIView, ActiveDriverAPIView, \
     ActiveVehicleDriverListAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     # ...
@@ -10,4 +12,7 @@ urlpatterns = [
     path('api/drivers/', DriverListAPIView.as_view(), name='driver-list'),
     path('api/vehicles/<int:vehicle_id>/active_driver/', ActiveDriverAPIView.as_view(), name='active-driver'),
     path('api/active-vehicles/', ActiveVehicleDriverListAPIView.as_view(), name='active-vehicles'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
